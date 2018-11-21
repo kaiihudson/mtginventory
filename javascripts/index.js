3b69833343e9030e23523a3bd092ddd147aca476
kaiihudson <sebaror1@gmail.com>
2018-11-21 13:47:48 -0300

    fix API

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,23 +1,33 @@
-const app = document.getElementById('root');
+//Get elements from HTML
+const 	app = document.getElementById('root'),
+		btn1 = document.getElementById('btn1'),
+		btn2 = document.getElementById('btn2'),
+		btn3 = document.getElementById('btn3');
 
+//Requests shortcut
+const request = new XMLHttpRequest();
+
+//Create elements on the HTML via JS
 const logo = document.createElement('img');
+const button = document.createElement('div');
+
+//Sources
 logo.src = './images/mainlogo.png';
 
-app.appendChild(logo);
-
-const request = new XMLHttpRequest();
 
 request.open('GET', 'https://api.magicthegathering.io/v1/cards', true);
 
+app.appendChild(logo);
 request.onload = function(){
 	//access JSON data on API
 	const data = JSON.parse(this.response);
+	console.log(data);
 	if (request.status >= 200 && request.status < 400){
-		data.forEach(cards => {
-				console.log('cards'.name);
+			data.cards.forEach(function(card) {
+				console.log(card);
 			});
 		} else {
-			conole.log("we've got an error in our hands")
+			console.log("we've got an error in our hands")
 		}
 	}
 
