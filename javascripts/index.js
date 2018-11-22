be74cc4ebf1df2c644dfda6cb17d3e3ee389a0d3
kaiihudson <sebaror1@gmail.com>
2018-11-22 13:49:30 -0300

    cambiando el name la api responde la carta que uno quiere

--- javascripts/index.js
+++ javascripts/index.js
@@ -8,7 +8,7 @@ const 	app = document.getElementById('root'),
 		btn3 = document.getElementById('btn3');
 
 //Requests shortcut
-const request = new XMLHttpRequest();
+// const request = new XMLHttpRequest();
 
 //Create elements on the HTML via JS
 const logo = document.createElement('img');
@@ -17,32 +17,37 @@ const button = document.createElement('div');
 //Sources
 logo.src = './images/mainlogo.png';
 
+app.appendChild(logo);
 
-/*const magicApi = require('mtgsdk')
+//WITH NODE.js (for production env?)
+/*
+const magicApi = require('mtgsdk')
 
 magicApi.card.all()
 .on('data', function(card) {
 	console.log(card.name)
-})*/
-request.open('GET', 'https://api.magicthegathering.io/v1/cards', true);
+})
+*/
+
+// WITH MTG API (response has only 100 registers, how do i dig deeper?)
+const header = new Headers();
+const inits= {
+	method: 'GET',
+	headers: header,
+	mode: 'cors',
+	cache: 'default'};
+function call() {
+	let cardname = prompt("card name", "card");
+	return cardname
+}
+const page = 'https://api.magicthegathering.io/v1/cards/?' + name
+fetch(page, inits)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(myJson);
+	});
 
 
-app.appendChild(logo);
-request.onload = function(){
-	//access JSON data on API
-	const response = JSON.parse(this.response);
-	const cards = response.cards;
-	//console.log(data)
-	if (request.status >= 200 && request.status < 400){
-		let search = cards.filter( card => card.name == 'Rancor');
-		console.log(search);
-
-		img1.appendChild('img')
-
-
-		} else {
-			console.log("we've got an error in our hands")
-		}
-	}
-request.send();
 
