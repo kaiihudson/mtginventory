a808a067ca24b4f8412c1be053e9e586da2a384c
kaiihudson <sebaror1@gmail.com>
2018-11-26 13:22:05 -0300

    i found a method for tables, yet it doesnt work /rjs

--- javascripts/search.js
+++ javascripts/search.js
@@ -1,25 +1,72 @@
 'use strict';
 
+const result = document.getElementById('results')
+
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
+class Search extends React.Component {
+  constructor(props){
     super(props);
-    this.state = { liked: false };
   }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+  render(){
+    'button',
+    { onClick:() => this.getdata()},
+    'Search'
+  };
 }
 
+const GeneralTable = React.createClass({
+  getdata: ()=> {
+    let name = prompt("cardname", "placeholder");
+    if (name != ""){
+      let page = 'https://api.magicthegathering.io/v1/cards/?name=' + name
+    } else {
+      alert("that's empty!")
+    }
+    return fetch(page, inits)
+            .then(status)
+            .then(json)
+            .then((data => {
+              if(prop){
+                rawData = JSON.stringify(data)
+              }
+            }))
+            .catch((error)=> {
+              console.log('Found an error', error)
+            })
+
+  },
+  generateRows: () => {
+  if(this.props.loading === false){
+      let cols = Object.keys(this.props.cards[0]), data = this.props.cards;
+      return data.map((item) => {
+        let cells = cols.map((colData) => {
+          return '<td> {cards[colData]} </td>';
+        });
+        return '<tr key={cards.id}> {cells} </tr>'
+      });
+    }
+  },
+  render:() => {
+    let rowComponents = this.generateRows();
+    let table_rows = [];
+    let table_headers = [];
+    let data = this.props.cards;
+
+    if (this.props.loading === false){
+      let headers = Objects.keys(this.props.cards[0]);
+      headers.forEach(header => table_headers.push('<th key={header}>{header}</th>'));
+      data.forEach(card => table_rows.push('<CardTableRow key={card.id} card={card} />'));
+    }
+  },
+  return (
+    <table striped bordered>
+      <thead><tr>{table_headers}</tr></thead>
+      <tbody>{rowComponents}</tbody>
+    </table>
+    );
+  },
+});
+
 const domContainer = document.querySelector('#search');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(Search), domContainer);
