2c51ceba0cdb40b73777d3738ed129722cb8d6b0
kaiihudson <sebaror1@gmail.com>
2018-11-26 22:30:11 -0300

    am lost

---
+++ javascripts/background.js
@@ -0,0 +1,39 @@
+import React {Component} from 'react';
+import ./test.css
+
+class Background extend Component {
+	constructor() {
+		super();
+		this.state = {
+			pictures: [],
+		};
+	}
+}
+
+ComponentDidMount(){
+	fetch('https://api.magicthegathering.io/v1/cards/')
+	.then(results => {
+		return results.json();	
+	}).then(data => {
+		let pictures = data.results.map((card) => {
+			return(
+				<div key={card.results}>
+					<img src={card.ImageUrl} />
+
+				</div>
+				)
+		})
+		this.setState({pictures: pictures});
+		console.log("state", this.state.pictures);
+	})
+}
+
+render(){
+	return(
+		<div className="container2">
+			<div className="container1">
+				{this.state.pictures}
+			</div>
+		</div>
+	)
+}
